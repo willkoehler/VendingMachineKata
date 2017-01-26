ca28f36605579c5a3dba0f0936cb3ff29558c584
Will Koehler <wck555@gmail.com>
2017-01-26 13:55:14 -0500

    Add a coin slot and coin return to VendingMachine

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,9 +1,15 @@
+require 'forwardable'
 require 'display'
+require 'coin_box'
 
 class VendingMachine
+  extend Forwardable
+  
+  delegate [:insert_coin, :check_coin_return] => :@coin_box
   
   def initialize
     @display = Display.new
+    @coin_box = CoinBox.new
   end
   
   def read_display
--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -9,4 +9,9 @@ describe VendingMachine do
     expect(vending_machine.read_display).to eq('INSERT COIN')
   end
   
+  it "should have a coin slot and coin return" do
+    vending_machine.insert_coin(weight: :nickel_weight, size: :nickel_size)
+    vending_machine.insert_coin(weight: :penny_weight, size: :penny_size)
+    expect(vending_machine.check_coin_return).to eq([{ weight: :penny_weight, size: :penny_size }])
+  end
 end
