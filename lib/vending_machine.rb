7231a153a924ca0820eedef0794ba575356d6c18
Will Koehler <wck555@gmail.com>
2017-01-26 14:02:27 -0500

    Update the display when coins are inserted

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -5,7 +5,7 @@ require 'coin_box'
 class VendingMachine
   extend Forwardable
   
-  delegate [:insert_coin, :check_coin_return] => :@coin_box
+  delegate [:check_coin_return] => :@coin_box
   
   def initialize
     @display = Display.new
@@ -14,5 +14,10 @@ class VendingMachine
   
   def read_display
     @display.read
+  end
+  
+  def insert_coin(coin)
+    @coin_box.insert_coin(coin)
+    @display.show_total_cash_inserted(@coin_box.total)
   end
 end
--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -14,4 +14,14 @@ describe VendingMachine do
     vending_machine.insert_coin(weight: :penny_weight, size: :penny_size)
     expect(vending_machine.check_coin_return).to eq([{ weight: :penny_weight, size: :penny_size }])
   end
+  
+  it "should update the display when coins are inserted" do
+    expect(vending_machine.read_display).to eq('INSERT COIN')
+    vending_machine.insert_coin(weight: :nickel_weight, size: :nickel_size)
+    expect(vending_machine.read_display).to eq('$0.05')
+    vending_machine.insert_coin(weight: :dime_weight, size: :dime_size)
+    expect(vending_machine.read_display).to eq('$0.15')
+    vending_machine.insert_coin(weight: :quarter_weight, size: :quarter_size)
+    expect(vending_machine.read_display).to eq('$0.40')
+  end
 end
